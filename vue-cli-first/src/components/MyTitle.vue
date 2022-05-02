d6c2b432c1363d4b34bb9a66416223718f0b4482
SirAlyon <alessiosada04@gmail.com>
2022-05-02 15:24:01 +0200

    add structure

---
+++ vue-cli-first/src/components/MyTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>La mia prima app con Vue Cli</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'MyTitle'
+}
+</script>
+
+<style scoped lang="scss">
+    h1{
+        text-align: center;
+        color: green
+    }
+</style>
